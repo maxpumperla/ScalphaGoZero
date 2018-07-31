d623fd194d963a7ec96a16e83cf296f552655917
Max Pumperla <max.pumperla@googlemail.com>
2018-07-31 12:14:04 +0200

    test setup

---
+++ src/test/scala/org/deeplearning4j/scalphagozero/agents/AgentTest.scala
@@ -0,0 +1,12 @@
+package org.deeplearning4j.scalphagozero.agents
+
+import org.scalatest.FunSpec
+
+class AgentTest extends FunSpec {
+
+  describe("An Agent") {
+    it("should have ...") {
+      assert(true)
+    }
+  }
+}
