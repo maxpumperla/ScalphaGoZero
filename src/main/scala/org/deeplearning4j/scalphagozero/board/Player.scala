3ea515ea378bec94cd12d6ab3fc8c1204d71c6f2
Max Pumperla <max.pumperla@googlemail.com>
2018-07-31 12:18:08 +0200

    go player

---
+++ src/main/scala/org/deeplearning4j/scalphagozero/board/Player.scala
@@ -0,0 +1,18 @@
+package org.deeplearning4j.scalphagozero.board
+
+/**
+  * Player of a game of Go (either black or white)
+  *
+  * @param color
+  */
+class Player(val color: Int) {
+
+  if (!color.equals(PlayerColor.black) && !color.equals(PlayerColor.black))
+    throw new IllegalArgumentException(s"Only black and white allowed as player. got $color")
+
+  def other: Player = if (this.color.equals(PlayerColor.black)) Player(PlayerColor.white) else Player(PlayerColor.black)
+}
+
+object Player {
+  def apply(color: Int) = new Player(color)
+}
