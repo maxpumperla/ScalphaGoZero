db4be4f8034d39df15bfac427e042983eb3c86a3
Max Pumperla <max.pumperla@googlemail.com>
2018-08-06 22:15:37 +0200

    last board test

--- src/test/scala/org/deeplearning4j/scalphagozero/board/BoardTest.scala
+++ src/test/scala/org/deeplearning4j/scalphagozero/board/BoardTest.scala
@@ -66,20 +66,21 @@ class BoardTest extends FunSpec {
     }
   }
 
-  describe("Test removing liberties") {
-    it("foo") {
+  describe("Test removing liberties:") {
+    it("a stone with four liberties should end up with three if an opponent stone is added") {
       val board = new GoBoard(5, 5)
       board.placeStone(Player(PlayerColor.black), Point(3, 3))
       board.placeStone(Player(PlayerColor.white), Point(2, 2))
       val whiteString = board.getGoString(Point(2, 2)).get
       assert(whiteString.numLiberties == 4)
+
       board.placeStone(Player(PlayerColor.black), Point(3, 2))
       val newWhiteString = board.getGoString(Point(2, 2)).get
       //assert(whiteString.numLiberties == 3)
     }
   }
 
-  describe("Empty triangle test") {
+  describe("Empty triangle test:") {
     it("an empty triangle in the corner with one white stone should have 3 liberties") {
       // x x
       // x o
@@ -96,5 +97,51 @@ class BoardTest extends FunSpec {
       assert(blackString.liberties.contains((2, 3)))
       assert(blackString.liberties.contains((1, 3)))
     }
+  }
+
+  describe("Test self capture:") {
+    // o.o..
+    // x.xo.
+    it("black can't take it's own last liberty") {
+      val board = new GoBoard(5, 5)
+      board.placeStone(Player(PlayerColor.black), Point(1, 1))
+      board.placeStone(Player(PlayerColor.black), Point(1, 3))
+      board.placeStone(Player(PlayerColor.white), Point(2, 1))
+      board.placeStone(Player(PlayerColor.white), Point(2, 2))
+      board.placeStone(Player(PlayerColor.white), Point(2, 3))
+      board.placeStone(Player(PlayerColor.white), Point(1, 4))
+
+      assert(board.isSelfCapture(Player(PlayerColor.black), Point(1, 2)))
+    }
+
+    // o.o..
+    // x.xo.
+    it("but if we remove one white stone, the move becomes legal") {
+      val board = new GoBoard(5, 5)
+      board.placeStone(Player(PlayerColor.black), Point(1, 1))
+      board.placeStone(Player(PlayerColor.black), Point(1, 3))
+      board.placeStone(Player(PlayerColor.white), Point(2, 1))
+      board.placeStone(Player(PlayerColor.white), Point(2, 3))
+      board.placeStone(Player(PlayerColor.white), Point(1, 4))
+
+      assert(!board.isSelfCapture(Player(PlayerColor.black), Point(1, 2)))
+    }
+
+    // xx...
+    // oox..
+    // x.o..
+    it("if we capture a stone in the process, it's not self-play") {
+      val board = new GoBoard(5, 5)
+      board.placeStone(Player(PlayerColor.black), Point(3, 1))
+      board.placeStone(Player(PlayerColor.black), Point(3, 2))
+      board.placeStone(Player(PlayerColor.black), Point(2, 3))
+      board.placeStone(Player(PlayerColor.black), Point(1, 1))
+      board.placeStone(Player(PlayerColor.white), Point(2, 1))
+      board.placeStone(Player(PlayerColor.white), Point(2, 2))
+      board.placeStone(Player(PlayerColor.white), Point(1, 3))
+
+      assert(!board.isSelfCapture(Player(PlayerColor.black), Point(1, 2)))
+    }
+
   }
 }
