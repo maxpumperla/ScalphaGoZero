24e40fae6a5fabe03e00fb50d0109e257dc260e4
Max Pumperla <max.pumperla@googlemail.com>
2018-08-22 14:35:38 +0200

    update

--- src/test/scala/org/deeplearning4j/scalphagozero/encoders/ZeroEncoderTest.scala
+++ src/test/scala/org/deeplearning4j/scalphagozero/encoders/ZeroEncoderTest.scala
@@ -1,7 +1,5 @@
 package org.deeplearning4j.scalphagozero.encoders
 
-import org.nd4j.linalg.factory.Nd4j
-import org.nd4j.linalg.indexing.NDArrayIndex
 import org.scalatest.FunSpec
 
 class ZeroEncoderTest() extends FunSpec {
